72c886fb686c33cc4f270db91876d240d3127389
indrajeet.nikam <indrajeet.nikam@siemens.com>
2021-02-19 12:26:04 +0530

    feat(header): add header

--- src/components/preview-collection/collection-preview.component.js
+++ src/components/preview-collection/collection-preview.component.js
@@ -3,21 +3,19 @@ import CollectionItem from '../collection-item/collection-item.component';
 
 import './collection-preview.styles.scss';
 
-const CollectionPreview = ({ title, items }) => {
-  return (
-    <div className='collection-preview'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
-      <div className='preview'>
-        {
-          items
-          .filter((items, idx) => idx < 4)
-          .map(({id, ...itemProps}) =>
-            <CollectionItem key={id} {...itemProps}/>
-          )
-        }
-      </div>
+const CollectionPreview = ({ title, items }) => (
+  <div className='collection-preview'>
+    <h1 className='title'>{title.toUpperCase()}</h1>
+    <div className='preview'>
+      {
+        items
+        .filter((items, idx) => idx < 4)
+        .map(({id, ...itemProps}) =>
+          <CollectionItem key={id} {...itemProps}/>
+        )
+      }
     </div>
-  )
-}
+  </div>
+)
 
 export default CollectionPreview;
