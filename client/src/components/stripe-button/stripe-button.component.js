eb4c07922dacf829f2170d855d110ad7127c1bc4
indrajeet.nikam <indrajeet.nikam@siemens.com>
2021-03-05 23:06:04 +0530

    feat(server): put entire front end app in client folder and added server at root level

--- client/src/components/stripe-button/stripe-button.component.js
+++ client/src/components/stripe-button/stripe-button.component.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
-
-const onToken = token => {
-  console.log(token);
-  alert(token);
-}
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_51IPYWYD4riJYucLd3sd52o2ogXwcNWk1IaVVbw1ZB3wIuLxQAoijKIjd1CYHtcSrocucPbzK9FXOf8jCVeGwOKvR00Meq3MPnq';
+
+  const onToken = token => {
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token: token
+      }
+    }).then(response => {
+      alert('Payment Successful!');
+    }).catch(error => {
+      console.warn('Payment error: ', JSON.parse(error));
+      alert('There was an issue with your payment. Please use provided credit card.');
+    })
+  }
 
   return (
     <StripeCheckout
