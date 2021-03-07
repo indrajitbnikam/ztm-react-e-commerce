d8987eb36ad66da9ed6472ee43c6f7c446a2c8fa
indrajeet.nikam <indrajeet.nikam@siemens.com>
2021-03-07 19:06:43 +0530

    feat: added error boundary

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import { createStructuredSelector } from "reselect";
 import { checkUserSession } from "./redux/user/user.actions";
 import { GlobalStyle } from "./global.styles";
 import Spinner from "./components/spinner/spinner.component";
+import ErrorBoundary from "./components/error-boundary/error-boundary.components";
 
 const HomePage = lazy(() => import('./pages/homepage/homepage.component'));
 const ShopPage = lazy(() => import('./pages/shop/shop.component'));
@@ -24,22 +25,24 @@ const App = ({ currentUser, checkUserSession }) => {
       <GlobalStyle />
       <Header />
       <Switch>
-        <Suspense fallback={<Spinner />}>
-          <Route exact path="/" component={HomePage}></Route>
-          <Route path="/shop" component={ShopPage}></Route>
-          <Route exact path="/checkout" component={CheckoutPage}></Route>
-          <Route
-            exact
-            path="/sign-in"
-            render={() =>
-              currentUser ? (
-                <Redirect to="/" />
-              ) : (
-                <SignInAndSignUpPage />
-              )
-            }
-          ></Route>
-        </Suspense>
+        <ErrorBoundary>
+          <Suspense fallback={<Spinner />}>
+            <Route exact path="/" component={HomePage}></Route>
+            <Route path="/shop" component={ShopPage}></Route>
+            <Route exact path="/checkout" component={CheckoutPage}></Route>
+            <Route
+              exact
+              path="/sign-in"
+              render={() =>
+                currentUser ? (
+                  <Redirect to="/" />
+                ) : (
+                  <SignInAndSignUpPage />
+                )
+              }
+            ></Route>
+          </Suspense>
+        </ErrorBoundary>
       </Switch>
     </div>
   );
---
+++ client/src/components/error-boundary/error-boundary.components.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { ErrorImageContainer, ErrorImageOverlay, ErrorImageText } from './error-boundary.styles';
+
+class ErrorBoundary extends Component {
+  state = {
+    hasError: false
+  };
+
+  static getDerivedStateFromError(error) {
+    // Process the errors
+
+    // Setting local state
+    return {
+      hasError: true
+    }
+  }
+
+  componentDidCatch(error, info) {
+    console.error(error, info);
+  }
+
+  render () {
+    return this.state.hasError
+    ? (
+      <ErrorImageOverlay>
+        <ErrorImageContainer imageUrl='https://i.imgur.com/yW2W9SC.png' />
+        <ErrorImageText>Sorry this page is broken 😥</ErrorImageText>
+      </ErrorImageOverlay>
+    )
+    : this.props.children;
+  }
+}
+
+export default ErrorBoundary;
