5367059430cf6fba9c329c44f2b3b31888671d7d
indrajeet.nikam <indrajeet.nikam@siemens.com>
2021-03-20 03:01:24 +0530

    feat(app): added pwa support

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const compression = require('compression');
+const enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', function(req, res) {
@@ -26,6 +28,10 @@ if (process.env.NODE_ENV === 'production') {
 app.listen(port, error => {
   if (error) throw error;
   console.log('Server running on port ' + port);
+})
+
+app.get('/service-worker.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
 })
 
 app.post('/payment', (req, res) => {
